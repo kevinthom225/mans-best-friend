196be0ccfc04100c8d3b7c765d769c63a6056c88
Kevin Thompson <kevin.thompson3189@gmail.com>
2020-11-18 19:06:03 -0500

    added comments

--- javascript/script.js
+++ javascript/script.js
@@ -1,34 +1,5 @@
 
-/*const name = document.getElementById("name");
-
-const email = document.getElementById("email");
-
-const form = document.getElementById("form");
-
-function validateForm (name, email) {
-    form.addEventListener("submit");
-    if(name.value === '' || email.value === ''){
-        alert('Both name and email required.');
-    }
-}
-
-validateForm(name.value, email.value);
-
-const emptySubmit = false;
-
-function isFieldEmpty(){
-    const nameField = document.querySelector('name');
-    const emailField = document.querySelector('email');
-    if(nameField.value === '' || emailField.value === ''){
-        return true;
-    }
-}
-
-emptySubmit = isFieldEmpty();
-
-if(emptySubmit){
-    alert('Both fields are required.')
-}*/
+/*Create array of prompt answers*/
 
 let answer1 = prompt('What are your 3 favorite breeds of dogs? Type 1: ');
 let answer2 = prompt('Type 2: ');
@@ -36,12 +7,16 @@ let answer3 = prompt('Type 3');
 
 const dogList = [answer1, answer2, answer3];
 
+/*Add div elements with the array answers*/ 
+
 const main = document.querySelector('main');
 let html = '';
 
 for (let i = 0; i < 3; i++){
     html += `<div>${dogList[i]}</div>`; 
 }
+
+/*Display the array on the website*/
 
 main.innerHTML = html;
 
